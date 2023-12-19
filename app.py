840610b8c8f16410b043e294cad25663ae3cca36
Arnab S <arnabsengupta104@gmail.com>
2023-12-19 17:52:14 +0530

    Minor cosmetic changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pipeline as pl
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="BhuvanRKSHA")
+st.set_page_config(page_title="BhuvanRKSHA", page_icon=":shield:")
 
 st.title("BhuvanRKSHA")
 
